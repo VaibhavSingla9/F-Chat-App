f1faa492ea1b7b174feb87e1de037c6665acc656
Vaibhav Singla <vaibhavsingla456@gmail.com>
2025-09-14 21:20:01 +0530

    Updated App.jsx, server.js and added bgImage.svg

--- server/server.js
+++ server/server.js
@@ -51,5 +51,13 @@ app.use("/api/messages" , messageRouter)
 // connect ot mongodb
  await connectDB();
 
-const PORT = process.env.PORT || 5000;
+ if(process.env.NODE_ENV !== "production")
+ {
+    const PORT = process.env.PORT || 5000;
 server.listen(PORT ,()=>console.log("Server is running on PORT: : " + PORT))
+ }
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT ,()=>console.log("Server is running on PORT: : " + PORT))
+
+// exporting server for vercel
+export default server;
